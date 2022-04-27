eca0ea00442dd22b310b418f7a2113a6c8ff755c
Partho <parthoo.rahman@gmail.com>
2022-04-27 20:00:10 +0600

    app.js

--- src/App.js
+++ src/App.js
@@ -7,20 +7,39 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Hello Partho
+          Hello React App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Component></Component>
+        <Person nayok="Manna" nayika="Tanna"></Person>
+        <Person nayok="bapparaz" nayika="Sabnaz"></Person>
+        <Person nayok="Jasim" nayika="Sabana"></Person>
+        <Person nayok="Salman" nayika="Sah"></Person>
       </header>
+
     </div>
   );
 }
 
+function Component() {
+  return (
+    <div style={{border: '2px solid red', margin:'12px', padding: '10px'}}>
+      <h1>I am H1 html tag</h1>
+      <h3>I am H3 html tag</h3>
+    </div>
+  )
+}
+
+function Person(props) {
+  const personStyle = {
+    border: '2px solid red', 
+    margin:'12px', 
+    padding: '10px'
+  }
+  return (
+    <div style = {personStyle}>
+      <h1>Name of Nayok: {props.nayok} </h1>
+      <h3>name of nayika: {props.nayika} </h3>
+    </div>
+  )
+}
 export default App;
