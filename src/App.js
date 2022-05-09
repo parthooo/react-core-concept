335a3dc00d8f68be62ee4353e7f895491b91fdbc
Partho <parthoo.rahman@gmail.com>
2022-05-09 20:45:20 +0600

    React compoment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+/*
 function App() {
   return (
     <div className="App">
@@ -42,4 +42,64 @@ function Person(props) {
     </div>
   )
 }
+*/
+
+
+function App() {
+  const nayoks = ['Anowar', 'Salman', 'Shakib', 'Manna'];
+  const products = [
+    {name: 'Photoshop', price : '$90.00'}, 
+    {name: 'Illustrator', price : '$100.00'},
+    {name: 'Adobe XD', price : '$70.00'}
+  ]
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello React App</p>
+
+        <Product product = {products[0]}></Product>
+        <Product product = {products[1]}></Product>
+        <Product product = {products[2]}></Product>
+
+
+        <Person name="Munna" job="Software Eng"></Person>
+        <Person name="Masum" job="Jr. Software Eng"></Person>
+      </header>
+
+    </div>
+  ); 
+}
+
+function Product(props) {
+  const productStyle={
+    border : '1px gray solid',
+    borderRadius : '5px',
+    backgroundColor: 'lightgray',
+    height: '250px',
+    width: '250px',
+    float: 'left',
+    display:'block'
+  }
+  const {name, price} = props.product;
+  console.log(name, price);
+  return(
+    <div style={productStyle}>
+      <h3> {name}</h3>
+      <h5> {price}</h5>
+      <button>Buy Now</button>
+    </div>
+  )
+}
+
+function Person(props) {
+  return (
+    <div style={{border: '2px solid red', margin:'12px', padding: '10px', float:'left', display:'block'}}>
+      <h3>Name: {props.name} </h3>
+      <p>Profession: {props.job}</p>
+    </div>
+  )
+}
+
+
 export default App;
